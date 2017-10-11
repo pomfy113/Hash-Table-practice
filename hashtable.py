dbc7e7001a06a50184e61a76286e0bf6e9bcf808
pomfy113 <pomfy113@gmail.com>
2017-10-11 16:12:26 -0700

    Operation speeds

--- hashtable.py
+++ hashtable.py
@@ -6,25 +6,28 @@ from linkedlist import LinkedList
 class HashTable(object):
 
     def __init__(self, init_size=8):
-        """Initialize this hash table with the given initial size"""
+        """Initialize this hash table with the given initial size."""
         self.buckets = [LinkedList() for i in range(init_size)]
 
     def __str__(self):
-        """Return a formatted string representation of this hash table"""
+        """Return a formatted string representation of this hash table."""
         items = ['{}: {}'.format(repr(k), repr(v)) for k, v in self.items()]
         return '{' + ', '.join(items) + '}'
 
     def __repr__(self):
-        """Return a string representation of this hash table"""
+        """Return a string representation of this hash table."""
         return 'HashTable({})'.format(repr(self.items()))
 
     def _bucket_index(self, key):
-        """Return the bucket index where the given key would be stored"""
+        """Return the bucket index where the given key would be stored."""
         return hash(key) % len(self.buckets)
 
     def keys(self):
-        """Return a list of all keys in this hash table"""
+        """Return a list of all keys in this hash table."""
         # Collect all keys in each of the buckets
+        # O(n)? You're grabbing every bucket, but also every
+        # item within the bucket. In terms of REAL time, it's n where
+        # n is the amount of total items which you spread out across buckets
         all_keys = []
         for bucket in self.buckets:
             for key, value in bucket.items():
@@ -32,8 +35,8 @@ class HashTable(object):
         return all_keys
 
     def values(self):
-        """Return a list of all values in this hash table"""
-        # TODO: Collect all values in each of the buckets
+        """Return a list of all values in this hash table."""
+        # O(n). See def keys(self) for thoughts.
         all_values = []
         for pail in self.buckets:
             for key, value in pail.items():
@@ -42,8 +45,8 @@ class HashTable(object):
         pass
 
     def items(self):
-        """Return a list of all items (key-value pairs) in this hash table"""
-        # Collect all pairs of key-value entries in each of the buckets
+        """Return a list of all items (key-value pairs) in this hash table."""
+        # O(n). See above again. Just about the same really.
         all_items = []
         for bucket in self.buckets:
             all_items.extend(bucket.items())
@@ -51,7 +54,9 @@ class HashTable(object):
 
     def length(self):
         """Return the length of this hash table by traversing its buckets"""
-        # Count number of key-value entries in each of the buckets
+        # O(n^2). Length is O(n), and we're looping through.
+        # TECHNICALLY it shouldn't change in terms of real time running;
+        # When all spread out in buckets vs in the same linked list is same.
         count = 0
         for pail in self.buckets:
             count += pail.length()
@@ -59,7 +64,8 @@ class HashTable(object):
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False"""
-        # Check if the given key exists in a bucket
+        # Check bucket O(1), then do a find O(n). It's O(n)
+        # Worst case scenario is if it's at the end of a bucket.
         if (self.buckets[self._bucket_index(key)].find(lambda item: item[0] == key)):
             return True
         else:
@@ -68,7 +74,7 @@ class HashTable(object):
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError"""
-        # Check if the given key exists and return its associated value
+        # See above. O(n) again.
         if self.contains(key):
             data = self.buckets[self._bucket_index(key)].find(lambda item: item[0] == key)
             return data[1]
@@ -80,6 +86,7 @@ class HashTable(object):
     def set(self, key, value):
         """Insert or update the given key with its associated value"""
         # Insert or update the given key-value entry into a bucket
+        # Constant + append which is constant.
         if self.contains(key):
             self.delete(key)
         self.buckets[self._bucket_index(key)].append((key, value))
@@ -87,7 +94,8 @@ class HashTable(object):
 
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError"""
-        # TODO: Find the given key and delete its entry if found
+        # See "delete" function of linked list;
+        # Can range from a few (if front or empty list) to O(n) (middle/end)
         if self.contains(key):
             self.buckets[self._bucket_index(key)].delete((key, self.get(key)))
             return
--- linkedlist.py
+++ linkedlist.py
@@ -6,19 +6,20 @@ from __future__ import print_function
 class Node(object):
 
     def __init__(self, data):
-        """Initialize this node with the given data"""
+        """Initialize this node with the given data."""
         self.data = data
         self.next = None
 
     def __repr__(self):
-        """Return a string representation of this node"""
+        """Return a string representation of this node."""
         return 'Node({})'.format(repr(self.data))
 
 
 class LinkedList(object):
 
     def __init__(self, iterable=None):
-        """Initialize this linked list; append the given items, if any"""
+        """Initialize this linked list; append the given items, if any."""
+        # O(n) per iterable; creation of head/tail are constants
         self.head = None
         self.tail = None
         if iterable:
@@ -31,11 +32,13 @@ class LinkedList(object):
 
     def items(self):
         """Return a list of all items in this linked list"""
+        # O(n); dependent on append making array is 1,
+        # assigning current is 1, looping through it + appending is 2n.
+
         result = []
         current = self.head
         while current is not None:
             result.append(current.data)
-            # result.append(current)
             current = current.next
         return result
 
@@ -45,7 +48,7 @@ class LinkedList(object):
 
     def length(self):
         """Return the length of this linked list by traversing its nodes"""
-        # TODO: count number of items
+        # O(n); 1 for accum, 1 per counter, 2n for the while loop
         accum = 0
         counter = self.head
         while counter:
@@ -55,8 +58,13 @@ class LinkedList(object):
         pass
 
     def append(self, item):
-        """Insert the given item at the tail of this linked list"""
-        # TODO: append given item
+        """Insert the given item at the tail of this linked list."""
+        # O(6); depends on how many inputs are in the linked list
+        # Best case: 6 operations
+        # Create node, assign .data as item (2). Check if empty (1).
+        # Apply self's head and tail into new_node.
+        # Worst case: still the same because we know where the tail is
+
         new_node = Node(item)
         if self.is_empty():
             self.head = self.tail = new_node
@@ -67,7 +75,9 @@ class LinkedList(object):
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list"""
-        # TODO: prepend given item
+        # See append; almost the same with things switched up
+        # O(6) total; constants are the same
+
         new_node = Node(item)
         if self.is_empty():
             self.head = self.tail = new_node
@@ -78,39 +88,53 @@ class LinkedList(object):
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError"""
-        # TODO: find given item and delete if found
+        # Hoo boy. It depends.
+        # See each bit for their individual; constant on here is 3
+
         current = self.head
         previous = None
         found = False
         while current:
             if current.data == item:
+                # If it's the last remaining node
+                # It's constant.
                 if (previous is None) and (current.next is None):
-                    current.next = None
-                    self.head = self.tail = None
+                    current.next = self.head = self.tail = None
                     found = True
                     break
+                # If it's at the head.
+                # Also constant. Best case due to slightly less operations
                 elif previous is None:
                     self.head = self.head.next
                     found = True
                     break
+                # If it's at the tail;
+                # man, if only I had double-linked list. It's O(n)
+                # Also the worst case scenario.
                 elif current.next is None:
                     self.tail = previous
                     previous.next = None
                     found = True
                     break
+                # All other cases
+                # Dependent on size of list and where we meet it
+                # Can be anywhere from one to a million
                 else:
                     previous.next = current.next
                     found = True
                     break
+            # Hey it's like extra 2
             previous = current
             current = current.next
+        # If it's not found, then it's 0(n); had to go through all of the list
         if not found:
             raise ValueError
         pass
 
     def find(self, quality):
-        """Return an item from this linked list satisfying the given quality"""
-        # TODO: find item where quality(item) is True
+        """Return item from this linked list satisfying the given quality."""
+        # O(n). Depends on how big the list is.
+        # Worst case is the entire list. Best case is meeting off the bat.
         counter = self.head
         while counter:
             if quality(counter.data):
